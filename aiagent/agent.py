ad3c8f3b2b7935651b4f420a20bd6ef43b4977df
phaelzin <rapha555lima@gmail.com>
2024-10-16 21:38:48 -0300

    Update agent.py

--- aiagent/agent.py
+++ aiagent/agent.py
@@ -1,4 +1,4 @@
 import os
 from langchain_openai import ChatOpenAI #importando o chatopenai da langchain\_openai
 from crewai import Agent, Task, Crew, Process
-from 
+from tools.searchtools import 
