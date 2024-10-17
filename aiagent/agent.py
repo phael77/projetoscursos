b1862e57659e26c68713a9b47529c135c3c9366b
phaelzin <rapha555lima@gmail.com>
2024-10-16 23:43:12 -0300

    alterações e instalações finalmente corretas

--- aiagent/agent.py
+++ aiagent/agent.py
@@ -1,4 +1,12 @@
 import os
-from langchain_openai import ChatOpenAI #importando o chatopenai da langchain\_openai
-from crewai import Agent, Task, Crew, Process
-from tools.searchtools import 
+from langchain_openai import ChatOpenAI
+from search_tools import SearchTools
+from dotenv import load_dotenv
+from crewai import Agent, Crew, Task, Process
+
+load_dotenv()
+
+serper_api_key = os.getenv("SERPER_API_KEY")
+
+if not serper_api_key:
+    raise ValueError("SERPER_API_KEY is not set in the environment variables.")
